b9fc7a2295b0931c37eb4745a2dfbcf109290198
DevSeok0414 <DevSeok0414@gmail.com>
2021-10-01 18:18:56 +0900

    Initial commit

---
+++ src/test/java/com/devseok0414/SpringMVCBoard/SpringMvcBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devseok0414.SpringMVCBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
