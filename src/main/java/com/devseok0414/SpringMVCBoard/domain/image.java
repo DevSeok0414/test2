f409bbe9bf4c9ffd2cc63b8eb1e26e590bbcacf5
DevSeok0414 <DevSeok0414@gmail.com>
2021-10-05 23:49:56 +0900

    domain annotation add

--- src/main/java/com/devseok0414/SpringMVCBoard/domain/image.java
+++ src/main/java/com/devseok0414/SpringMVCBoard/domain/image.java
@@ -1,5 +1,12 @@
 package com.devseok0414.SpringMVCBoard.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString @NoArgsConstructor
 public class image {
     private String id;
     private String board_id;
--- src/main/java/com/devseok0414/SpringMVCBoard/domain/reply.java
+++ src/main/java/com/devseok0414/SpringMVCBoard/domain/reply.java
@@ -1,7 +1,14 @@
 package com.devseok0414.SpringMVCBoard.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.Date;
 
+@Getter @Setter
+@ToString @NoArgsConstructor
 public class reply {
     private String id;
     private String board_id;
