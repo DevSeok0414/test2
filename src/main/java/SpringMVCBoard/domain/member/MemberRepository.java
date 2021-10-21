3a3321beb455818c024c00e82ef83bdb3b5aa739
DevSeok0414 <DevSeok0414@gmail.com>
2021-10-21 14:16:18 +0900

    etc..

---
+++ src/main/java/SpringMVCBoard/domain/member/MemberRepository.java
@@ -0,0 +1,30 @@
+package SpringMVCBoard.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<String, Member> store = new HashMap<>();   //static 사용
+
+    public Member save(Member member) {
+        log.info("save: member={}", member);
+        store.put(member.getUserId(), member);
+        return member;
+    }
+
+    public Member findById(String userId) {
+        return store.get(userId);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
